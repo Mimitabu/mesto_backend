2434fdf751576f936066171f1b719abd5f559d78
Marie Slavina <morefly@yandex.ru>
2019-11-28 18:19:19 +0300

    Add email validation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +18,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     match: /(https?:\/\/)(www\.)?(((\d{1,3}\.){3}\d{1,3})|((\w+)((-\w+)+)?\.\w{2,4}))((\.\w+)+)?(:\d{2,5})?((\/+\w+)+#?)?/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'It is not valid email',
+    },
+  },
+  password: {
+    tipe: String,
+    required: true,
+    minlength: 8,
   },
 });
 
