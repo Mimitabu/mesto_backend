2081763204e320a5156a13f841581958a1e16c8f
Marie Slavina <morefly@yandex.ru>
2019-12-12 21:16:35 +0300

    fix card url

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const {
   getUsers, getUserById, updateUser, updateAvatar,
 } = require('../controllers/users');
 
-routerUsers.get('/users/users', getUsers);
+routerUsers.get('/users', getUsers);
 routerUsers.get('/users/:userId', getUserById);
 routerUsers.patch('/users/me', updateUser);
 routerUsers.patch('/users/me/avatar', updateAvatar);
